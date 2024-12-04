03659a716a28ede212c9cdff985049b74d41d442
Antonio da Silva <antonio.silva@raizeducacao.com.br>
2024-12-04 20:58:56 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
-  //MOSTRA A VERSÃO DO JQUERY NO CONSOLE
-  console.log("Versão do JQuery: ", $.fn.jquery);
-  ocultaCancelar();
+    //MOSTRA A VERSÃO DO JQUERY NO CONSOLE
+    console.log("Versão do JQuery: ", $.fn.jquery);
 
-    // Seleciona todos os elementos com a classe '.card-title'
-    $('.card-title').each(function() {
+
+    // Verifica se o jQuery está funcionando e se os elementos existem
+    console.log('Existem', $('.card-title').length, 'elementos .card-title na página.');
+
+    // Verifica se os elementos estão presentes antes de tentar manipulá-los
+    if ($('.card-title').length === 0) {
+        console.log('Nenhum elemento com a classe .card-title encontrado.');
+    }
+
+    // Itera sobre todos os títulos com a classe '.card-title'
+    $('.card-title').each(function () {
+        // Verifica o título atual
+        console.log('Verificando título:', $(this).text());
+
         // Verifica se o texto do título começa com '[Atendimento]'
         if ($(this).text().startsWith('[Atendimento]')) {
             // Cria a imagem
@@ -16,8 +27,12 @@ $(document).ready(function () {
 
             // Adiciona a imagem antes do texto do título
             $(this).prepend(icon);
+
+            // Exibe o título que recebeu o ícone no console
+            console.log('Título com ícone: ' + $(this).text());
         }
     });
+    ocultaCancelar();
 
 });
 
